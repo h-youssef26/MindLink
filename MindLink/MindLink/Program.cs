e562d2cea2974d8c545108361ea8111401ec055c
Mohamed Youssef <mohamed.youssef@agile-architecture.com>
2025-04-13 00:33:34 +0200

    added new file

--- MindLink/MindLink/Program.cs
+++ MindLink/MindLink/Program.cs
@@ -1,17 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MindLink.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MindLinkDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("AppDbConnectionString"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("AppDbConnectionString"))));
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
